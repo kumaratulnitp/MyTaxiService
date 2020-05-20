033f85ff593745f26d36c69e37c1e3bc3f51c51a
Kumar Atul <kumaratulnit@gmail.com>
2020-05-20 22:36:41 +0530

    Skeleton

---
+++ src/main/java/com/personal/mytaxiservice/MyTaxi/MyTaxiApplication.java
@@ -0,0 +1,13 @@
+package com.personal.mytaxiservice.MyTaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTaxiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/mytaxiservice/MyTaxi/MyTaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.mytaxiservice.MyTaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
